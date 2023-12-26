830ba02c0762773f6f2c890c43b56972587efc31
morgan nordberg <morno368@student.liu.se>
2023-12-26 15:25:07 +0100

    day 2 finished

--- 2020/day2/day2.c
+++ 2020/day2/day2.c
@@ -4,11 +4,12 @@
 
 int partOne();
 int countOccurrences(char c, char *arr);
-
+int partTwo();
 
 int main(){
     printf("%d", partOne());
     printf("\n");
+    printf("%d", partTwo());
     return 0;
 }
 
@@ -29,7 +30,7 @@ int partOne(){
 
         while (line[i] != '-') {
             lowerBound *= 10;
-            lowerBound += line[i] - '0';
+            lowerBound += line[i] - '0'; //ASCII value of '0' ... '9' start at 47, ex '5' - '0' is equivolent to 52 - 47
             i++;
         }
         i++; // Move past '-'
@@ -45,7 +46,6 @@ int partOne(){
         c = line[i]; // Get the character to count
         i += 3; // Move past the character and ':'
 
-        // Count occurrences in the rest of the string
         int occurrences = countOccurrences(c, &line[i]);
 
         if (occurrences >= lowerBound && occurrences <= upperBound) {
@@ -64,4 +64,44 @@ int countOccurrences(char c, char *arr) {
         }
     }
     return sum;
+}
+
+
+int partTwo(){
+    FILE *file_ptr;
+    file_ptr = fopen("day2_input.txt", "r");
+    char line[MAX_LENGTH];
+
+    int validPasswords = 0;
+    while (fgets(line, MAX_LENGTH, file_ptr))
+    {
+        int firstIndex = 0;
+        int secondIndex = 0;
+        char c;
+        int i = 0;
+
+
+        while (line[i] != '-') {
+            firstIndex *= 10;
+            firstIndex += line[i] - '0';
+            i++;
+        }
+        i++; // Move past '-'
+
+        while (line[i] != ' ') {
+            secondIndex *= 10;
+            secondIndex += line[i] - '0';
+            i++;
+        }
+        i += 1; // Move past ' '
+
+        c = line[i]; // Get the character to compare
+        i += 2; // Move past the character and ':'
+
+        //XOR operator ^
+        if (line[i + firstIndex] == c ^ line[i + secondIndex] == c) {
+            validPasswords++;
+        }
+    }
+    return validPasswords;
 }
